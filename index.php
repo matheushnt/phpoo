c360d22ec939e3c0c1942cab1fc9ef5c56cb5404
Matheus Henrique <matheushnt.dev@gmail.com>
2026-01-19 10:17:41 -0300

    Exercício 1: primeira classe

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
+
+use src\classes\Product;
+
+$ps5 = new Product;
+$ps5->name = 'PlayStation 5';
+$ps5->price = 2899.99;
+$ps5->stock = 4;
+
+dump($ps5);
---
+++ src/classes/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace src\classes;
+
+/*
+Crie uma classe Produto com as propriedades públicas nome, preco e estoque.
+Instancie um objeto e exiba suas informações.
+*/
+
+class Product
+{
+    public string $name;
+    public float $price;
+    public int $stock;
+}
